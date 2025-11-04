134645c844c610bca0d13ecf5888f0738d1def9c
hichamSalhi <hicham.salhi@usmba.ac.ma>
2025-11-04 11:55:53 +0100

    fix: Changing the API base url

--- frontend/src/services/taskService.ts
+++ frontend/src/services/taskService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://task-management-pbp3.onrender.com';
 
 export interface Task {
   id: number;
